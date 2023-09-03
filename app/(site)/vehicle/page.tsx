3f51a1b171aead392bd5f1e59f8534512bf40df1
MUHAMAD_IZZUDDIN_BIN_AZHAM_00129756 <muhamadizzuddin.az@etiqa.com.my>
2023-09-03 11:25:11 +0800

    push 124

--- app/(site)/vehicle/page.tsx
+++ app/(site)/vehicle/page.tsx
@@ -61,7 +61,8 @@ export default function Vehicle() {
               </tr>
             </thead>
             <tbody className="divide-y divide-gray-100">
-            {/* {addFlexItem.map((row, i) => {
+            {
+            /* {addFlexItem.map((row, i) => {
                 return (
                     <tr className="bg-white">
                     <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
