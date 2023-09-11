bcb8ea72e71cc2145d0f84413b15795edf196a0c
MUHAMAD_IZZUDDIN_BIN_AZHAM_00129756 <muhamadizzuddin.az@etiqa.com.my>
2023-09-11 23:49:29 +0800

    push 11092023

--- app/(site)/analysis/page.tsx
+++ app/(site)/analysis/page.tsx
@@ -2,6 +2,8 @@
 // import React, { useEffect, useRef } from "react";
 // import Chart from "chart.js/auto";
 
+import BarChart from "@/app/component/bar-chart";
+
 // export default function App() {
 //     const canvasEl = useRef(null);
   
@@ -78,96 +80,12 @@
   
 
 export default function App() {
-  
+  return (
+<div>
+  <BarChart/>
+</div>
+  )
 }
 
 
 
-// 'use client'
-
-// import React from 'react';
-// import {
-//   Chart as ChartJS,
-//   CategoryScale,
-//   LinearScale,
-//   BarElement,
-//   Title,
-//   Tooltip,
-//   Legend,
-// } from 'chart.js';
-// import { Bar } from 'react-chartjs-2';
-// import { faker } from '@faker-js/faker';
-// import { useState, useEffect } from "react";
-
-// ChartJS.register(
-//   CategoryScale,
-//   LinearScale,
-//   BarElement,
-//   Title,
-//   Tooltip,
-//   Legend
-// );
-
-// export const options = {
-//   plugins: {
-//     title: {
-//       display: true,
-//       text: 'Chart.js Bar Chart - Stacked',
-//     },
-//   },
-//   responsive: true,
-//   interaction: {
-//     mode: 'index' as const,
-//     intersect: false,
-//   },
-//   scales: {
-//     x: {
-//       stacked: true,
-//     },
-//     y: {
-//       stacked: true,
-//     },
-//   },
-// };
-
-// const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-// export const data = {
-//   labels,
-//   datasets: [
-//     {
-//       label: 'Dataset 1',
-//       data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-//       backgroundColor: 'rgb(255, 99, 132)',
-//       stack: 'Stack 0',
-//     },
-//     {
-//       label: 'Dataset 2',
-//       data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-//       backgroundColor: 'rgb(75, 192, 192)',
-//       stack: 'Stack 0',
-//     },
-//     {
-//       label: 'Dataset 3',
-//       data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-//       backgroundColor: 'rgb(53, 162, 235)',
-//       stack: 'Stack 1',
-//     },
-//   ],
-// };
-
-// const BarChart = () => {
-//   const [chartData, setChartData] = useState({
-//     dataSets: [],
-//   });
-
-//   const [chartOptions, setChartOptions] = useState({});
-//   return (
-//     <div className="w-full md:col-span-2 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white">
-//    <Bar options={options} data={data} />;
-
-//     </div>
-//   );
-// };
-
-// export default BarChart;
--- app/component/chart/bar-chart.tsx
+++ app/component/chart/bar-chart.tsx
@@ -1,3 +1,88 @@
-export default function App() {
-    
-}
+'use client'
+
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
+import { useState, useEffect } from "react";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  plugins: {
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart - Stacked',
+    },
+  },
+  responsive: true,
+  interaction: {
+    mode: 'index' as const,
+    intersect: false,
+  },
+  scales: {
+    x: {
+      stacked: true,
+    },
+    y: {
+      stacked: true,
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      backgroundColor: 'rgb(255, 99, 132)',
+      stack: 'Stack 0',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      backgroundColor: 'rgb(75, 192, 192)',
+      stack: 'Stack 0',
+    },
+    {
+      label: 'Dataset 3',
+      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      backgroundColor: 'rgb(53, 162, 235)',
+      stack: 'Stack 1',
+    },
+  ],
+};
+
+const BarChart = () => {
+  const [chartData, setChartData] = useState({
+    dataSets: [],
+  });
+
+  const [chartOptions, setChartOptions] = useState({});
+  return (
+    <div className="w-full md:col-span-2 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white">
+   <Bar options={options} data={data} />;
+
+    </div>
+  );
+};
+
+export default BarChart;
