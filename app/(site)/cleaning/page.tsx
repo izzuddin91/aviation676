9d5a708d39b7f01427a3f3e6290c16a9ff9a2717
MUHAMAD_IZZUDDIN_BIN_AZHAM_00129756 <muhamadizzuddin.az@etiqa.com.my>
2024-06-29 16:13:16 +0800

    push 29 jun 2024

--- app/(site)/cleaning/page.tsx
+++ app/(site)/cleaning/page.tsx
@@ -84,46 +84,76 @@ export default function Cleaning() {
   }
 
   return (
-    <div className="p-5 h-screen bg-gray-100">
-    <Dialog
-      open={open}
-      onClose={handleClose}
-      aria-labelledby="alert-dialog-title"
-      aria-describedby="alert-dialog-description"
-    >
-      <DialogTitle id="alert-dialog-title">Image</DialogTitle>
-      <DialogContent>
-        <DialogContentText id="alert-dialog-description">
-          <img src={fileName} alt="" />
-        </DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose}>Dismiss</Button>
-      </DialogActions>
-    </Dialog>
-    <div className="grid grid-cols gap-4 p-4">
-    Upcoming cleaning
-    {carParts.map((row: any, i: number) => {
-      return ( <div key={i}>asd</div> )
-    })}
-      <div className="overflow-auto rounded-lg shadow">
-        
-        <table className="w-full">
-          <thead className="bg-gray-50 border-b-2 border-gray-200">
-            <tr>
-              <th className="w-20 p-3 text-sm font-semibold tracking-wide text-left">
-                No.
-              </th>
-              <th className="p-3 text-sm font-semibold tracking-wide text-left">
-                House Name
-              </th>
-              <th className="w-32 p-3 text-sm font-semibold tracking-wide text-left">
-                Date
-              </th>
-            </tr>
-          </thead>
-          <tbody className="divide-y divide-gray-100">
-            {carParts.map((row: any, i: number) => (
+    <div className="p-2 space-y-10">
+      <div id="container2"></div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">Details</DialogTitle>
+
+        <DialogActions>
+          <Button onClick={handleClose}>Dismiss</Button>
+        </DialogActions>
+      </Dialog>
+      <div>
+        <Button
+          style={{ margin: "10px" }}
+          variant="outlined"
+          onClick={() => router.back()}
+        >
+          Back
+        </Button>
+
+      </div>
+      <div>
+      </div>
+      <table className="w-full">
+        <thead className="bg-gray-50 border-b-2 border-gray-200">
+          <tr>
+            <th className="w-20 p-3 text-sm font-semibold tracking-wide text-left">
+              No.
+            </th>
+            <th className="w-24 p-3 text-sm font-semibold tracking-wide text-left">
+              Expenses
+            </th>
+            <th className="w-24 p-3 text-sm font-semibold tracking-wide text-left">
+              Revenue
+            </th>
+            <th className="w-24 p-3 text-sm font-semibold tracking-wide text-left">
+              Date
+            </th>
+            <th className="w-24 p-3 text-sm font-semibold tracking-wide text-left">
+              Notes
+            </th>
+            <th className="w-24 p-3 text-sm font-semibold tracking-wide text-left">
+              File
+            </th>
+            <th className="w-24 p-3 text-sm font-semibold tracking-wide text-left">
+              Edit
+            </th>
+            <th className="w-24 p-3 text-sm font-semibold tracking-wide text-left">
+              Delete
+            </th>
+          </tr>
+        </thead>
+        <tbody className="divide-y divide-gray-100">
+          {carParts.map((row: any, i: number) => {
+            let bgColor = "";
+            switch (row["category"]) {
+              case "grocery":
+                bgColor = "text-green-800 bg-green-200";
+                break;
+              case "cleaning":
+                bgColor = "text-blue-800 bg-blue-200";
+                break;
+              case "damage":
+                bgColor = "text-red-800 bg-red-200";
+                break;
+            }
+            return (
               <tr key={i} className="bg-white">
                 <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
                   <a
@@ -134,43 +164,43 @@ export default function Cleaning() {
                   </a>
                 </td>
                 <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
-                  {row["houseName"]}
+                  <span
+                    className={`p-1.5 text-xs font-medium uppercase tracking-wider ${bgColor} rounded-lg bg-opacity-50`}
+                  >
+                    <span> {row["profitBeforeAdminCharge"]} </span>
+                  </span>
+                </td>
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                  <span
+                    className={`p-1.5 text-xs font-medium uppercase tracking-wider ${bgColor} rounded-lg bg-opacity-50`}
+                  >
+                    <span> {row["revenue"]} </span>
+                  </span>
                 </td>
                 <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
                   {row["date"]
                     ? moment(row["date"].toDate()).format("DD-MM-YYYY")
                     : ""}
                 </td>
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                </td>
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                  <Button
+                    endIcon={<PhotoIcon />}
+                    onClick={() => {
+                      handleClickOpen(row);
+                    }}
+                  ></Button>
+                </td>
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                </td>
+                <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                </td>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      {/* <div className="grid grid-cols-1 gap-1">
-        Upcoming Cleaning List
-        {carParts.map((row, i) => (
-          <div key={i} className="bg-white p-4 rounded-lg shadow">
-            <div className="flex items-center space-x-2 text-sm">
-              <div>Date: </div>
-              <div>
-                <strong>
-                  {row["date"]
-                    ? moment(row["date"].toDate()).format("DD-MM-YYYY")
-                    : ""}
-                </strong>
-              </div>
-            </div>
-            <div className="flex items-center space-x-2 text-sm">
-              <div>Location: </div>
-              <div>
-                <strong>{row["houseName"]}</strong>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div> */}
-
+            );
+          })}
+        </tbody>
+      </table>
     </div>
-  </div>
   );
 }
