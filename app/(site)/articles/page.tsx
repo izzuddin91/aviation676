156fd4faf02f23b60d17a1ed41e4dc7f88c3c164
MUHAMAD_IZZUDDIN_BIN_AZHAM_00129756 <muhamadizzuddin.az@etiqa.com.my>
2024-09-28 10:00:02 +0800

    push 28 sept 2024

--- app/(site)/articles/page.tsx
+++ app/(site)/articles/page.tsx
@@ -24,46 +24,46 @@ export default function Articles() {
     router.push("/articles/" + articleId);
   }
 
-  return(
-    <div>
-      <VideoBackground />
-    </div>
-  )
-  // return (
-  //   <div className="grid lg:grid-cols-6 gap-4 p-4">
-  //     {articles ? (
-  //       articles.map((article: any, i: any) => {
-  //         return (
-  //           <div
-  //             key={i}
-  //             className="lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg"
-  //           >
-  //             <div className="flex flex-col w-full pb-4">
-  //               <p className="text-2xl font-bold">
-  //                 { article["title"] }
-  //               </p>
-  //               <br />
-  //               <p className="text-gray-600">
-  //                 { (`${article["para1"]}`).substring(0, 200) + "..." }
-  //               </p>
-  //               <br />
-  //               <p className="bg-violet-200 flex justify-center items-center p-3 rounded-lg">
-  //                 <button
-  //                   onClick={() => {
-  //                     articleDetails(article["id"]);
-  //                   }}
-  //                   className="text-violet-700 text-lg"
-  //                 >
-  //                   Read More
-  //                 </button>
-  //               </p>
-  //             </div>
-  //           </div>
-  //         );
-  //       })
-  //     ) : (
-  //       <LoadingIndicator />
-  //     )}
+  // return(
+  //   <div>
+  //     <VideoBackground />
   //   </div>
-  // );
+  // )
+  return (
+    <div className="grid lg:grid-cols-6 gap-4 p-4">
+      {articles ? (
+        articles.map((article: any, i: any) => {
+          return (
+            <div
+              key={i}
+              className="lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg"
+            >
+              <div className="flex flex-col w-full pb-4">
+                <p className="text-2xl font-bold">
+                  { article["title"] }
+                </p>
+                <br />
+                <p className="text-gray-600">
+                  { (`${article["para1"]}`).substring(0, 200) + "..." }
+                </p>
+                <br />
+                <p className="bg-violet-200 flex justify-center items-center p-3 rounded-lg">
+                  <button
+                    onClick={() => {
+                      articleDetails(article["id"]);
+                    }}
+                    className="text-violet-700 text-lg"
+                  >
+                    Read More
+                  </button>
+                </p>
+              </div>
+            </div>
+          );
+        })
+      ) : (
+        <LoadingIndicator />
+      )}
+    </div>
+  );
 }
--- app/component/VideoBackground.js
+++ app/component/VideoBackground.js
@@ -1,18 +1,18 @@
-import React from 'react';
+// import React from 'react';
 
-const VideoBackground = () => {
-    return (
-      <div className='videoContainer'>
-        <video autoPlay muted loop >
-          <source src="/videos/sample.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        <div >
-          <h1 style={{"color": "white"}}>Welcome to LazyFat</h1>
-        </div>
-      </div>
-    );
-  };
+// const VideoBackground = () => {
+//     return (
+//       <div className='videoContainer'>
+//         <video autoPlay muted loop >
+//           <source src="/videos/sample.mp4" type="video/mp4" />
+//           Your browser does not support the video tag.
+//         </video>
+//         <div >
+//           <h1 style={{"color": "white"}}>Welcome to LazyFat</h1>
+//         </div>
+//       </div>
+//     );
+//   };
   
-  export default VideoBackground;
+//   export default VideoBackground;
   
